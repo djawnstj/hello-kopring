c19ad0120f1c88c527b8d2dab2378f0f1b266444
junseo <djawnstj@naver.com>
2022-08-31 10:56:18 +0900

    init

---
+++ src/main/kotlin/com/travelit/TravelitTestApplication.kt
@@ -0,0 +1,11 @@
+package com.travelit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TravelitTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<TravelitTestApplication>(*args)
+}
